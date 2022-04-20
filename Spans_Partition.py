3684fdc5aae57347e2aff09f9a08bcf1d66efbd3
Omri-Rafaeli <omri.rafaeli@ibm.com>
2022-04-20 14:57:30 +0300

    Spans Partition works good, the span ends in the second entity

--- Spans_Partition.py
+++ Spans_Partition.py
@@ -1,15 +1,21 @@
 #This files gets a text file with no stop words and partioning this text to spans with maximum 200 tokens.
 # a token beggins 100 tokens before a mention of a entity (or with a different entity) and ends 100 tokens
 # after the reference or with another entity. A span will not include more than 2 entities.
-import pandas as pd
-import pickle
-import csv
 
+#this function is dedicated to find if a word is from the 2d names array,
+# if so it returns the entity index, for example, if an entity is called Yonatan Lifshitz and another one is called Yolek,
+# "Yonatan" and "Lifshitz" will return the same value, but "Lifshitz" and "Yolek" will return different int values.
+# the word "Table" will return false, because it is not a name from the names 2d list.
+def index_2d(names2d, appearance):
+    for i, x in enumerate(names2d):
+        if appearance in x:
+            return i
+    return False
 
 def generate_span(text,first_entity_idx,last_span_end_idx,names):
-
     l=len(text)
     if(first_entity_idx<l):
+        first_entity_in_entities_array = index_2d(names,text[first_entity_idx])
         characters=[]
         characters.append("1 "+text[first_entity_idx])
     #the first word of the next span is eather 100 words ago or the last span end if it is closer
@@ -24,24 +30,34 @@ def generate_span(text,first_entity_idx,last_span_end_idx,names):
     i=first_entity_idx
     if (i >= l):
         return [], i
-    while i<first_entity_idx+100 and not (text[i] in names):
+
+    entity_appear = index_2d(names, text[i])
+    #while i didnt get 100 tokens after the first entity, and i didnt see a DIFFERENT entity
+    while i<first_entity_idx+100 and (entity_appear is False or entity_appear==first_entity_in_entities_array):
         cur_span.append(text[i])
         i+=1
         if(i>=l):
             break
-    if not (text[i] in names):
+        entity_appear = index_2d(names, text[i])
+    #if I didnt find another entity in the next 100 tookens after the first entity so this is not a good span
+    if i>=first_entity_idx+100:
         return False, False
-    second_entity=0
-    if (text[i] in names):
-        second_entity=1
-        while text[i] in names or ((not text[i] in names) and i<first_entity_idx+100):
-            if(text[i] in names and second_entity==1):
+    # if i found the second entity which is not identical to the first one
+    if entity_appear is not False:
+        # while i didnt find the second entity or i found and entity identical to the first one
+        while entity_appear is not False or (entity_appear is False and i<first_entity_idx+100):
+            if(entity_appear is not False and entity_appear!=first_entity_in_entities_array):
                 characters.append("2 " +text[i])
-            else:
-                second_entity=0
+                cur_span.append(text[i])
+                break
             cur_span.append(text[i])
             i+=1
+            entity_appear = index_2d(names,text[i])
+
+        # appending the characters in this span to the output file, and the length of thi span
         cur_span.append(characters)
+        len_of_span = i-cur_first_word_idx
+        cur_span.append(len_of_span)
         return cur_span,i
 
 
@@ -58,9 +74,11 @@ for line in file1:
 file1.close()
 
 flat_entities = [] #a flat list of all the entities
+i=0
 for sublist in entities:
     for item in sublist:
         flat_entities.append(item)
+    i+=1
 print("Done!")
 
 
@@ -82,21 +100,30 @@ spans=[]
 spans_counter=0
 last_span_end_idx = 0
 while i<len(words):
-    while not (words[i] in flat_entities):
+    entity_appear = index_2d(entities,words[i])
+    while entity_appear is False:
         i+=1
         if(i>=len(words)):
             break
-    cur_span,i =generate_span(words,i,last_span_end_idx,flat_entities)
+        entity_appear = index_2d(entities, words[i])
+
+    first_character_idx = i
+    cur_span,i =generate_span(words,i,last_span_end_idx,entities)
     last_span_end_idx=i
     i+=1
-    if cur_span!=False:
+    if cur_span is not False:
         spans.append(cur_span)
+    else:
+        last_span_end_idx = first_character_idx+100
+        i=first_character_idx+100
+    #if end of the book, print to ouput file
     if i>=len(words):
         with open('PerfectPeace_Spans/spans_PerfectPeace.txt', 'w',encoding="mbcs") as f:
             for item in spans:
                 for word in item:
                     f.write("%s " % word)
                 f.write("\n")
+            f.write(str(len(spans)) + " Spans")
         f.close()
         break
 
