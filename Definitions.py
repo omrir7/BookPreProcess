09afa8b30b70acfc6abd0b89ec3eb251cba3ad2e
Omri Rafaeli <omrir7@gmail.com>
2022-12-28 17:32:03 +0200

    changed definitions paths to be relative

--- Definitions.py
+++ Definitions.py
@@ -3,16 +3,16 @@
 book_name = "PerfectPeace"
 
 #book .txt file path - input to Remove_StopWrods
-book_txt_path = "C:/Users/OmriRafaeli/PycharmProjects/NLP/BookNLP/A Perfect Peace/A Perfect Peace.txt"
+book_txt_path = "../BookNLP/A Perfect Peace/A Perfect Peace.txt"
 
 #book .txt file without stop words
-no_stop_words_path = 'C:/Users/OmriRafaeli/PycharmProjects/NLP/BookNLP/A Perfect Peace/A Perfect Peace - NoStopWords.txt'
+no_stop_words_path = '../BookNLP/A Perfect Peace/A Perfect Peace - NoStopWords.txt'
 
 #raw names file - Input od Parsing_Names
-raw_names_file_path = "C:/Users/OmriRafaeli/PycharmProjects/NLP/BookNLP/A Perfect Peace/outputdir/Names"
+raw_names_file_path = "../BookNLP/A Perfect Peace/outputdir/Names"
 
 #names parsed path - output of Parsing_Names
-parsed_names_path = 'C:/Users/OmriRafaeli/PycharmProjects/NLP/BookNLP/A Perfect Peace/outputdir/Names_Parsed'
+parsed_names_path = '../BookNLP/A Perfect Peace/outputdir/Names_Parsed'
 #how many characters will be evaluated (number of most referenced entities that will be evaluated)
 num_of_entities = 3
 
@@ -30,11 +30,11 @@ min_spans = 7
 #minimu, length of span
 min_span_length = 5
 #Metadata Path
-Metadata_Path = "C:/Users/OmriRafaeli/PycharmProjects/NLP/rmn/data/metadata.pkl"
+Metadata_Path = "../rmn/data/metadata.pkl"
 
 #csv path
-csv_path = "C:/Users/OmriRafaeli/PycharmProjects/NLP/rmn/data/relationships.csv"
+csv_path = "../rmn/data/relationships.csv"
 
 #NER TESTS
-flair_ner_file = "C:/Users/OmriRafaeli/PycharmProjects/NLP/BookNLP/A Perfect Peace/outputdir/Names_flair.pkl"
-spacy_ner_file = "C:/Users/OmriRafaeli/PycharmProjects/NLP/BookNLP/A Perfect Peace/outputdir/Names_spacy.pkl"
+flair_ner_file = "../BookNLP/A Perfect Peace/outputdir/Names_flair.pkl"
+spacy_ner_file = "../BookNLP/A Perfect Peace/outputdir/Names_spacy.pkl"
